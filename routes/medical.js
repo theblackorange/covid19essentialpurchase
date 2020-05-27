ea30d1e48cb42d70e9390e805c9b95a16bb43040
K R Venkatesh <venky.venkatesh24@gmail.com>
2020-05-27 19:54:20 +0530

    routes added

---
+++ routes/medical.js
@@ -0,0 +1,10 @@
+const express= require('express');
+const router=express.Router();
+const mongoose=require('mongoose');
+
+const medicalModel=require('../model/medicalModel');
+
+
+
+
+module.exports=router;
---
+++ routes/seller.js
@@ -0,0 +1,10 @@
+const express= require('express');
+const router=express.Router();
+const mongoose=require('mongoose');
+
+const sellerModel=require('../model/sellerModel');
+
+
+
+
+module.exports=router;
